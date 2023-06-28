77ae398069012de4b41dabcacfe0676c5a738d77
Cha Yuhyun <caca518@kaist.ac.kr>
2023-06-28 10:49:02 +0900

    ! Modify Self-Play Reignforcement Algorithm to use Neural Network
    * Modify Self-Play Reignforcement Algorithm to use Neural Network
    * Modify check_winner method algorithm : check only surround of last move
    * Some comment has made for understanding code
    
    ! Committed by Cha Yuhyun
    ! Date : 2023/06/28

--- NN.py
+++ NN.py
@@ -6,16 +6,20 @@ __author__ = "Cha Yuhyun"
 __email__ = "caca518@kaist.ac.kr"
 __Last_Modify__ = "June 27, 2023"
 
-
 import numpy as np
 import random
+import torch
+import torch.nn as nn
+import torch.optim as optim
 
 class RenjuGame:
     def __init__(self, board_size):
         self.board_size = board_size
         self.board = np.zeros((board_size, board_size), dtype=np.int)
-        self.current_player = 1
+        self.current_player = 1                                             # current_player = 1 or 2
         self.winner = None
+        self.last_move_col = 0
+        self.last_move_row = 0
 
     def is_valid_move(self, row, col):
         return self.board[row][col] == 0
@@ -25,22 +29,35 @@ class RenjuGame:
             self.board[row][col] = self.current_player
             self.check_winner()
             self.current_player = 3 - self.current_player
+            self.last_move_row = row
+            self.last_move_col = col
 
-    def check_winner(self):
-        directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]  # horizontal, vertical, diagonal, anti-diagonal
-        for dr, dc in directions:
-            for row in range(self.board_size):
-                for col in range(self.board_size):
-                    if self.board[row][col] != 0:
-                        color = self.board[row][col]
-                        win = True
-                        for i in range(5):
-                            if row + i * dr < 0 or row + i * dr >= self.board_size or col + i * dc < 0 or col + i * dc >= self.board_size or self.board[row + i * dr][col + i * dc] != color:
-                                win = False
-                                break
-                        if win:
-                            self.winner = color
-                            return
+ def check_winner(self):
+     directions = [(0, 1), (1, 0), (1, 1), (-1, 1)]  # horizontal, vertical, diagonal, anti-diagonal
+     
+     last_row, last_col = self.last_move_row, self.last_move_col
+     last_color = self.board[last_row][last_col]
+ 
+     for dr, dc in directions:
+         win = True
+         for i in range(-4, 5):
+             row = last_row + i * dr
+             col = last_col + i * dc
+             if (
+                 row < 0
+                 or row >= self.board_size
+                 or col < 0
+                 or col >= self.board_size
+                 or self.board[row][col] != last_color
+             ):
+                 win = False
+                 break
+ 
+         if win:
+             self.winner = last_color
+             return
+ 
+    
 
     def is_game_over(self):
         return np.count_nonzero(self.board) == self.board_size * self.board_size or self.winner is not None
@@ -115,9 +132,29 @@ class MCTSNode:
                 best_child = child
         return best_child.state.get_valid_moves()[0]
 
+class PolicyNetwork(nn.Module):
+    def __init__(self, board_size):
+        super(PolicyNetwork, self).__init__()
+        self.board_size = board_size
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)    # First convolutional layer with input channels=1 and output channels=32
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)   # Second convolutional layer with input channels=32 and output channels=64
+        self.fc1 = nn.Linear(64 * (board_size ** 2 // 4), 256)               # Fully connected layer with input features=64*(board_size^2/4) and output features=256
+        self.fc2 = nn.Linear(256, board_size ** 2)                           # Fully connected layer with input features=256 and output features=board_size^2
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))        # Apply ReLU activation to the output of the first convolutional layer
+        x = torch.relu(self.conv2(x))        # Apply ReLU activation to the output of the second convolutional layer
+        x = x.view(x.size(0), -1)            # Reshape x into a 2D matrix with size (batch_size, -1)
+        x = torch.relu(self.fc1(x))          # Apply ReLU activation to the output of the first fully connected layer
+        x = self.fc2(x)                      # Output the final logits from the second fully connected layer
+        return x
+
 class MCTSAgent:
-    def __init__(self, iterations):
+    def __init__(self, iterations, board_size):
         self.iterations = iterations
+        self.board_size = board_size
+        self.policy_network = PolicyNetwork(board_size)
+        self.optimizer = optim.Adam(self.policy_network.parameters(), lr=0.001)
 
     def get_action(self, state):
         root = MCTSNode(state)
@@ -139,29 +176,49 @@ class MCTSAgent:
             node = node.select_best_child(c_param=1.0 / np.sqrt(2))
         return node
 
-def self_play(agent):
-    game = RenjuGame(board_size=15)
-    while not game.is_game_over():
-        move = agent.get_action(game)
-        game.make_move(move[0], move[1])
-    return game.winner
+    def self_play(self):
+        game = RenjuGame(board_size=self.board_size)
+        states = []
+        while not game.is_game_over():
+            state_tensor = torch.tensor(game.get_state().reshape(1, 1, self.board_size, self.board_size), dtype=torch.float32)
+            states.append(state_tensor)
+            move = self.get_action(game)
+            game.make_move(move[0], move[1])
+        winner = game.winner
+        labels = torch.zeros(len(states), self.board_size ** 2)
+        if winner != 0:
+            for i in range(len(states)):
+                labels[i][move[0] * self.board_size + move[1]] = 1 # label : i th move is what, move represented by 1 dimension
+        return states, labels
+
+    def train(self, num_games):
+        for _ in range(num_games):
+            states, labels = self.self_play()
+            inputs = torch.cat(states, dim=0)
+            targets = torch.cat(labels, dim=0)
+            self.optimizer.zero_grad()
+            outputs = self.policy_network(inputs)
+            loss = nn.BCEWithLogitsLoss()(outputs, targets)
+            loss.backward()
+            self.optimizer.step()
 
 # Training
-iterations = 1000
-agent = MCTSAgent(iterations)
+iterations = 10
+agent = MCTSAgent(iterations, board_size=15)
 
-num_games = 100
-wins = 0
-losses = 0
-draws = 0
+num_games = 100                                 
+agent.train(num_games)
 
-for _ in range(num_games):
-    winner = self_play(agent)
-    if winner == 1:
-        wins += 1
-    elif winner == 2:
-        losses += 1
-    else:
-        draws += 1
+# Testing
+game = RenjuGame(board_size=15)
+while not game.is_game_over():
+    state_tensor = torch.tensor(game.get_state().reshape(1, 1, 15, 15), dtype=torch.float32)
+    output = agent.policy_network(state_tensor)
+    probabilities = torch.sigmoid(output)
+    valid_moves = game.get_valid_moves()
+    probabilities = probabilities.squeeze().detach().numpy()
+    valid_probabilities = probabilities.reshape(15, 15)[np.array(valid_moves)[:, 0], np.array(valid_moves)[:, 1]]
+    best_move = valid_moves[np.argmax(valid_probabilities)]
+    game.make_move(best_move[0], best_move[1])
 
-print(f"Wins: {wins}, Losses: {losses}, Draws: {draws}")
+print(f"Winner: {game.winner}")
