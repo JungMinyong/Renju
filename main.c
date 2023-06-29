0d0317b84e21e58201681bf39282119adf8ec817
Cha Yuhyun <caca518@kaist.ac.kr>
2023-06-29 19:48:03 +0900

    ! Fix win message bug
    * fix the bug that win message doesn't popup immediately when computer win
    * add CheckWinDebug function to winfunc.c
    
    ! Author : Cha yuhyun

--- main.c
+++ main.c
@@ -3,12 +3,9 @@
 #include "board.h"
 #include "winfunc.h"
 #include "mcts.h"
-#include <mpi.h>
 
 int main(int argc, char *argv[]) {
-    MPI_Init(&argc, &argv);
     int rank;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);    
 
     char column;
     int row;
@@ -19,32 +16,34 @@ int main(int argc, char *argv[]) {
     your_color = 1; //you are black stone
 
 
-    while(1) {
+    bool gameOver = false;
+   
+    while (!gameOver) {
         if (state.current_stone == your_color) {
             print_board(&state);
             printf("Enter coordinates (e.g., h8): ");
             scanf(" %c%d", &column, &row);
             add_stone(&state, column, row);
-	    state.current_stone = (state.current_stone == BLACK) ? WHITE : BLACK;
+            state.current_stone = (state.current_stone == BLACK) ? WHITE : BLACK;
         } else {
             int max_search = 10000;
-	    int best_action = monte_carlo_tree_search(state, max_search);
-	    print_board(&state);
-	    make_move(&state, best_action);
-	    //add_stone_computer(&state);
+            int best_action = monte_carlo_tree_search(state, max_search);
+            make_move(&state, best_action);
         }
-        if (checkWin(&state)){
+   
+   
+        if (checkWinDebug(&state)) {
             print_board(&state);
-            printf("%d win the game\n", state.current_stone);
-            break;
-        }
-        if (checkDraw(&state)){
+            printf("Player %d wins the game\n", 3 - state.current_stone);
+            printf("Congratulations!\n");
+            gameOver = true;
+        } else if (checkDraw(&state)) {
             print_board(&state);
             printf("There is no valid place - Draw");
-            break;
+            gameOver = true;
         }
-        //get_valid_moves() would need a different implementation if you intend to use it, as the current version returns an array, which is not valid in C.
+   
     }
-    MPI_Finalize();
-    return 0;
+   
+     return 0;
 }
